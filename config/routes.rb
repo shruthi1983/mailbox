b3147a97cbd91304c38bb8d37f7fb3331560b542
shruthi <shruthirmgm@gmail.com>
2021-12-09 17:12:19 +0530

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+ devise_for:users
+
+
+ resources :conversations do
+  resourses :messages
 end
+ root 'conversations#index'
+ 
+
