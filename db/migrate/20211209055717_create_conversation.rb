4d81041f52eeeb5add4e589acb389656340ec596
shruthi <shruthirmgm@gmail.com>
2021-12-09 11:49:10 +0530

    created migrations for converations and messages

---
+++ db/migrate/20211209055717_create_conversation.rb
@@ -0,0 +1,8 @@
+class CreateConversation < ActiveRecord::Migration[6.1]
+  def change
+    create_table :conversations do |t|
+    t.integer :sender_id
+    t.integer :recipient_id
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2021_12_09_061144) do
+
+  create_table "conversations", force: :cascade do |t|
+    t.integer "sender_id"
+    t.integer "recipient_id"
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.text "body"
+    t.integer "conversation_id"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["conversation_id"], name: "index_messages_on_conversation_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
+  end
 
 end
